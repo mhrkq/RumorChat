6e2d180c05636eb1fcc8875eadc3b87e1314b451
Bryan Tan <bryantanzy@gmail.com>
2023-10-19 14:27:00 +0800

    Update export_to_csv.py

--- export_to_csv.py
+++ export_to_csv.py
@@ -4,9 +4,10 @@ import sys
 import shutil 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
 # Import the app instance and your models from your main module
-from main import app, Rooms, Messages, ChatbotMessages
+from main import app, db, Rooms, Messages, ChatbotMessages
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
@@ -14,7 +15,8 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
-db = SQLAlchemy()
+# db = SQLAlchemy()
+from main import db
 
 def export_to_csv():
     # Push an application context
@@ -50,15 +52,39 @@ def export_to_csv():
 def zip_data_folder():
     shutil.make_archive("data", 'zip', "data")
 
+def reset_database():
+    with engine.connect() as connection:
+        for table in [Rooms.__tablename__, Messages.__tablename__, ChatbotMessages.__tablename__]:
+            print(f"Dropping table {table}...")
+            with connection.begin():
+                connection.execute(text(f"DROP TABLE IF EXISTS {table} CASCADE;"))
+            print(f"Table {table} dropped.")
+        
+        # Now that tables are dropped, let's recreate them using Flask's app context.
+        with app.app_context():
+            print("Recreating tables...")
+            db.create_all()
+            print("Database reset completed!")
+
 if __name__ == "__main__":
+    # Initiate CSV export regardless of whether the reset-db flag is given
     print("Initiating CSV export...")
     csv_files = export_to_csv()
 
     print("\nZipping the data folder...")
     zip_data_folder()
     print("Data folder zipped as 'data.zip'")
-
+    
     print("\nCSV export completed!")
     print("You can find the exported files at the following locations:")
     for file in csv_files:
         print(os.path.abspath(file))
+
+    # Check if the reset-db flag is given
+    if 'reset-db' in sys.argv:
+        print("\nWARNING: You have opted to reset the database. This will DELETE all data!")
+        choice = input("Are you sure you want to continue? [yes/no]: ")
+        if choice.lower() == 'yes':
+            reset_database()
+        else:
+            print("Database reset operation aborted by the user.")
